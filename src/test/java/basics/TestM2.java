04af0d638c3d9bfa10ce286d734757ff2723e615
Sunil <sunil.r@testyantra.com>
2023-08-28 15:58:56 +0530

    Added My New FrameWork

---
+++ src/test/java/basics/TestM2.java
@@ -0,0 +1,24 @@
+package basics;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestM2 {
+	
+	WebDriver driver;
+	@Test
+	public void testCaseID012() {
+		
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("http://www.amazon.com");
+		
+	}
+
+}
+
